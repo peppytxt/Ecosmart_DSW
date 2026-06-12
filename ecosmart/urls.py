ae8bd685ee4906a18610cea8ec5bb38a3e78ee97
peppytxt <pedrocn236@gmail.com>
2026-06-12 11:29:15 -0400

    urls está dentro de ecosmart

--- ecosmart/urls.py
+++ ecosmart/urls.py
@@ -4,7 +4,7 @@ from ecosmart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('ecosmart.urls')),
+    path('api/', include('ECOSMART-DSW-MAIN.urls')),
     path('api/usuarios/', views.api_usuarios, name='api_usuarios'),
     path('api/conteudos/', views.api_conteudos, name='api_conteudos'),
     path('api/signup/', views.api_signup, name='api_signup'),
